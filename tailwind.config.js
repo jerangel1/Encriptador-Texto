8a22b9fa6d85420e58a1e3c37d7c3c61feb37e71
Jesus Rangel <jerangel1691@gmail.com>
2024-03-10 12:04:09 -0400

    Actualizar estilos de tarjetas y configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,45 @@
 module.exports = {
   theme: {
     extend: {
-      animation: {
-        'slide': 'slide 1s ease-out forwards',
+      colors: {
+        'gradient-light': '#00ff75',
+        'gradient-dark': '#3700ff',
       },
-      keyframes: {
-        slide: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(100%)' },
-        }
-      }
-    }
+      boxShadow: {
+        card: '0px 0px 30px 1px rgba(0, 255, 117, 0.30)',
+      },
+      width: {
+        '90vw': '90vw',
+        '80vw': '80vw',
+        '100vw': '100vw',
+      },
+      height: {
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh',
+      },
+      maxWidth: {
+        '190px': '190px',
+      },
+      maxHeight: {
+        '254px': '254px',
+      },
+      borderRadius: {
+        '20px': '20px',
+      },
+      transitionProperty: {
+        'all': 'all',
+      },
+      transitionDuration: {
+        '0.2s': '0.2s',
+        '0.3s': '0.3s',
+      },
+      transform: {
+        'scale-0.98': 'scale(0.98)',
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [require('tailwindcss'),
+  require('autoprefixer'),],
 }
